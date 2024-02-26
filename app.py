4d7060bd355008b5025df9509fc98d98e482817e
Per Fogelin <42115215+danribba@users.noreply.github.com>
2024-02-26 21:23:57 +0100

    Uppdatering av namnet

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from stryktipset import *
   
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-app = Flask(__name__, template_folder='templates', static_folder='statics')
+my_app = Flask(__name__, template_folder='templates', static_folder='statics')
   
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@app.route('/')
+@my_app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 
 
@@ -41,6 +41,6 @@ if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(debug="true")   
+    my_app.run(debug="true")   
 
 
